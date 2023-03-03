27af1bb6f356d1f53533d798687971b3490b2997
Raihan <rhnchdri@gmail.com>
2023-03-03 22:58:00 +0600

    user created

--- src/Components/Registers/SignUP/SignUp.js
+++ src/Components/Registers/SignUP/SignUp.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../Context/AuthProvider';
 
 const SignUp = () => {
     const { register, formState: { errors }, handleSubmit } = useForm();
+    const { createUser } = useContext(AuthContext);
+
     const handleSignIn = data => {
         console.log(data)
+        createUser(data.email, data.password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(err => console.error(err));
     }
     return (
         <div className='flex justify-center items-center my-20'>
--- src/Context/AuthProvider.js
+++ src/Context/AuthProvider.js
@@ -1,14 +1,21 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 import app from '../firebase/firebase.config';
-import { getAuth } from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    // create user
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
 
     const authInfo = {
-
+        createUser
     };
 
     return (
