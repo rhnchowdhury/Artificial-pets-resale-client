5b07d39f39bc33559bc5706647a4b975c592f25d
Raihan <rhnchdri@gmail.com>
2023-03-03 09:37:53 +0600

    login created

--- src/Components/Registers/Login/Login.js
+++ src/Components/Registers/Login/Login.js
@@ -1,8 +1,48 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const { register, handleSubmit } = useForm();
     return (
-        <div>
+        <div className='flex justify-center items-center my-20'>
+            <div>
+                <form onSubmit={handleSubmit()}>
+                    <div>
+                        <label className="label"><span className="label-text">Name</span></label>
+                        <input type='text' {...register("name",
+                            {
+                                required: "Name is required"
+                            })}
+                            className="input input-bordered w-full max-w-xs" />
+                        {/* {errors.name && <p className='text-error'>{errors.name?.message}</p>} */}
+                    </div>
+                    <div>
+                        <label className="label"><span className="label-text">Email</span></label>
+                        <input type='text' {...register("email",
+                            {
+                                required: "Email address is required"
+                            })}
+                            className="input input-bordered w-full max-w-xs" />
+                        {/* {errors.email && <p className='text-error'>{errors.email?.message}</p>} */}
+                    </div>
+                    <div>
+                        <label className="label"><span className="label-text">Password</span></label>
+                        <input type='password' {...register("password",
+                            {
+                                required: "Password is required",
+                                minLength: { value: 6, message: 'Password must be 6 characters or longer' },
+                                pattern: { value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/, message: "Password must be strong" }
+                            })}
+                            className="input input-bordered w-full max-w-xs" />
+                        {/* {errors.password && <p className='text-error'>{errors.password?.message}</p>} */}
+                        <label className="label"><span className="label-text">Forget password?</span></label>
+                    </div>
+                    <input className='btn btn-active w-full max-w-xs' value='Login' type="submit" />
+                    {/* {loginError && <p className='text-error'>{loginError}</p>} */}
+                    <p>New to mobile garage? <Link to='/signup' className='text-orange-600 font-bold'>Create an account</Link></p>
+                </form>
+            </div>
 
         </div>
     );
