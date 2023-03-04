bb0bd92efc13184990eb3988920b91416c0ce3cb
Raihan <rhnchdri@gmail.com>
2023-03-04 09:27:55 +0600

    navBar edited

--- src/Components/Shared/Headers/Header.js
+++ src/Components/Shared/Headers/Header.js
@@ -12,20 +12,19 @@ const Header = () => {
     };
 
     const menuItems = <React.Fragment>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/blog'>Blog</Link></li>
+        <li><Link to='/' className='text-white' style={{ background: '#F9575C', borderRadius: '355px 45px 225px 75px/15px 225px 15px 255px' }}>Home</Link></li>
+        <li><Link to='/blog' className='text-white' >Blog</Link></li>
         {user?.uid ?
             <>
-                <li><Link to='/dashboard' className=''>Dashboard</Link></li>
-                <li><Link onClick={handleLogOut}>Sign Out</Link></li>
+                <li><Link to='/dashboard' className='text-white'>Dashboard</Link></li>
+                <li><Link onClick={handleLogOut} className='text-white'>Sign Out</Link></li>
             </>
             :
-            <li><Link to='/login' className='\'>Log in</Link></li>}
+            <li><Link to='/login' className='text-white'>Log in</Link></li>}
     </React.Fragment>
     return (
         <div>
             <div className="navbar" style={{ background: '#F9BE4F' }}>
-                {/* #F9BE4F */}
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -35,7 +34,7 @@ const Header = () => {
                             {menuItems}
                         </ul>
                     </div>
-                    <Link className="btn btn-ghost normal-case text-xl">daisyUI</Link>
+                    <Link className="btn btn-ghost normal-case text-xl text-white">Artificial Pets</Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
