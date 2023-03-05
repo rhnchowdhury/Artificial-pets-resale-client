0a72713baad18b69197557df2d49c671578da3fd
Raihan <rhnchdri@gmail.com>
2023-03-06 02:12:07 +0600

    booking saved in backend

--- src/Components/Pages/Bookings/BookModal.js
+++ src/Components/Pages/Bookings/BookModal.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { toast } from 'react-hot-toast';
 import { AuthContext } from '../../../Context/AuthProvider';
 
 const BookModal = ({ booked, setBooked }) => {
@@ -23,7 +24,22 @@ const BookModal = ({ booked, setBooked }) => {
             petsPrice: price,
             phoneNumber: phone,
             Address: address
-        }
+        };
+
+        fetch('http://localhost:5000/booking', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(booking)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.acknowledged) {
+                    setBooked(null);
+                    toast.success('Item is booked')
+                }
+            })
 
     };
 
