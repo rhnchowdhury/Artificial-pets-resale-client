65ca0baad507d35c3334843ce8e738ce7fe779fc
Raihan <rhnchdri@gmail.com>
2023-03-07 00:14:46 +0600

    token created

--- src/Components/Layout/DashboardLayout.js
+++ src/Components/Layout/DashboardLayout.js
@@ -6,6 +6,9 @@ const DashboardLayout = () => {
     return (
         <div>
             <Header></Header>
+            <label tabIndex={0} htmlFor="dashboard-drawer" className="btn btn-ghost lg:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </label>
             <div className="drawer drawer-mobile">
                 <input id="dashboard-drawer" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content">
--- src/Components/Pages/Dashboard/MyOrders/MyOrders.js
+++ src/Components/Pages/Dashboard/MyOrders/MyOrders.js
@@ -11,7 +11,11 @@ const MyOrders = () => {
     const { data: booking = [] } = useQuery({
         queryKey: ['booking', user?.email],
         queryFn: async () => {
-            const res = await fetch(url)
+            const res = await fetch(url, {
+                headers: {
+                    authorization: `bearer ${localStorage.getItem('accessToken')}`
+                }
+            })
             const data = await res.json();
             return data;
         }
