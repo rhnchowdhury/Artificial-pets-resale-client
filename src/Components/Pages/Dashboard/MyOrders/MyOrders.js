7f51a92999336271389df9fa56e8c3d0f19c2e79
Raihan <rhnchdri@gmail.com>
2023-03-06 15:02:36 +0600

    query tanstack used

--- src/Components/Pages/Dashboard/MyOrders/MyOrders.js
+++ src/Components/Pages/Dashboard/MyOrders/MyOrders.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../../Context/AuthProvider';
 
 const MyOrders = () => {
+    const { user } = useContext(AuthContext);
+
+    const url = `http://localhost:5000/booking?email=${user?.email}`;
+
+    const { data: booking = [] } = useQuery({
+        queryKey: ['booking', user?.email],
+        queryFn: async () => {
+            const res = await fetch(url)
+            const data = await res.json();
+            return data;
+        }
+    })
+
     return (
         <div>
             <h1 className='text-2xl font-bold text-orange-600 text-center my-12'>My Orders</h1>
@@ -15,6 +31,17 @@ const MyOrders = () => {
                             <th>Pay Option</th>
                         </tr>
                     </thead>
+                    <tbody>
+                        {
+                            booking.map((books, i) => <tr key={books._id}>
+                                <th>{i + 1}</th>
+                                <td>{books.petsName}</td>
+                                <td><img src={books.petsImg} className='w-12 h-12' alt='' /></td>
+                                <td>{books.petsPrice}</td>
+                                <Link to='/dashboard/payment'> <button className='btn btn-outline mt-2'>Payment</button></Link>
+                            </tr>)
+                        }
+                    </tbody>
                 </table>
             </div>
         </div>
