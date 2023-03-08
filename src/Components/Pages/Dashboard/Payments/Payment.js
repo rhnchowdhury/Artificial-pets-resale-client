81c45883171fb5f6efd4d6d9ca625e80db9f2df4
Raihan <rhnchdri@gmail.com>
2023-03-08 12:21:49 +0600

    payment implement

--- src/Components/Pages/Dashboard/Payments/Payment.js
+++ src/Components/Pages/Dashboard/Payments/Payment.js
@@ -1,9 +1,25 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import CheckOutForm from './CheckOutForm';
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
+console.log(stripePromise)
 
 const Payment = () => {
+    const pay = useLoaderData();
+    const { petsName, petsPrice } = pay;
+
     return (
         <div>
-            <h1>thi is payment</h1>
+            <h1>Payment for {petsName}</h1>
+            <p>Please pay {petsPrice} for your pets.</p>
+            <div className='w-96 my-10 border-4 p-8'>
+                <Elements stripe={stripePromise}>
+                    <CheckOutForm pay={pay} />
+                </Elements>
+            </div>
         </div>
     );
 };
--- src/Components/Route/Route.js
+++ src/Components/Route/Route.js
@@ -54,7 +54,8 @@ const router = createBrowserRouter([
             },
             {
                 path: '/dashboard/payment/:id',
-                element: <Payment></Payment>
+                element: <Payment></Payment>,
+                loader: ({ params }) => fetch(`http://localhost:5000/booking/${params.id}`)
             },
             {
                 path: '/dashboard/add',
